27052e05c0eeaf0679e2748e1dcd835f05b4b075
sourabh kumar <sourabhkumar20212021@gmail.com>
2023-08-24 19:42:43 +0530

    change in the file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,13 @@ module ApplicationHelper
   def set_layout
    is_namespace_admin ? "admin" : "application"
   end
+
+  def flash_class(level)
+    case level
+    when 'notice' then "alert alert-info"
+    when 'success' then "alert alert-success"
+    when 'error' then "alert alert-error"
+    when 'alert' then "alert alert-error"
+    end
+  end
 end
