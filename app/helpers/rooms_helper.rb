03e7c253c03b2a3d05ad1794850fc2f5c7f825a2
sourabh kumar <sourabhkumar20212021@gmail.com>
2023-11-22 22:48:07 +0530

    change in view for chat

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,22 @@
 module RoomsHelper
+
+	def chat_room
+		agent_id = []
+		ids = User.agents.map(&:id)
+		# rooms = Room.where(user_id: current_user.id, sender_id: ids)
+		ids.each do |id|
+			room = Room.where(user_id: current_user.id, sender_id: id) || Room.where(user_id: id, sender_id: current_user.id)
+			if !room.present?
+				agent_id << id
+			end
+		end
+		# debugger
+		user = User.where(id: agent_id)
+		return user.last
+	end
+
+	def profile_details(id)
+		user = User.find_by(id: id)
+		return user
+	end
 end
